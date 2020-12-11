01fda91a90ed3077e78de295731767899803ae3e
FlorianBEME <florianbme@gmail.com>
2020-12-12 00:05:24 +0100

    add algorythm capitalise

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -2,8 +2,21 @@ const assert = require('assert');
 
 
 // Check that capitalizeFirst transforms javaScript correctly
-assert.strictEqual(capitalizeFirst('javaScript test hello'), 'JavaScript Test Hello');
+assert.strictEqual(capitalizeFirst('javaScript test hello'), 'Javascript Test Hello');
 // Check that it works for a 1-character string
 assert.strictEqual(capitalizeFirst('z'), 'Z');
 // Check that it works for an empty string
 assert.strictEqual(capitalizeFirst(''), '');
+
+
+function capitalizeFirst(str) {
+    if (str.length <= 0) {
+      return "";
+    } else {
+      return str
+        .toLowerCase()
+        .split(" ")
+        .map((x) => x[0].toUpperCase() + x.slice(1))
+        .join(" ");
+    }
+  }
