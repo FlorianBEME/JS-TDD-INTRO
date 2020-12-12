0cf0b54122453fce5c5481422b927e309feb8429
FlorianBEME <florianbme@gmail.com>
2020-12-12 17:24:10 +0100

    a

---
+++ rectangle.js
@@ -0,0 +1,21 @@
+class Rectangle {
+    constructor(a, b) {
+      this.a = a;
+      this.b = b;
+    }
+  
+    isSquare() {
+  
+    }
+  
+    getArea() {
+  
+    }
+  
+    getPerimeter() {
+  
+    }
+  }
+  
+  module.exports = Rectangle;
+  
